c6111d6fdf8f0d365209d3be46d48831b9cf7134
xiaweizi <xiaweizi@xiaomi.com>
2019-07-26 17:35:43 +0800

    [dev] testActivity 入口添加

--- app/src/main/java/com/xiaweizi/myapplication/MainActivity.java
+++ app/src/main/java/com/xiaweizi/myapplication/MainActivity.java
@@ -29,5 +29,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ServerActivity.class));
             }
         });
+        findViewById(R.id.bt_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
     }
 }
