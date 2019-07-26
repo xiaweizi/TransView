2002368aca6cc1df1a2096396555975918c424c8
xiaweizi <xiaweizi@xiaomi.com>
2019-07-26 09:51:51 +0800

    [dev] 添加客户端服务端入口

---
+++ app/src/main/java/com/xiaweizi/myapplication/ServerActivity.java
@@ -0,0 +1,14 @@
+package com.xiaweizi.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server);
+    }
+}
