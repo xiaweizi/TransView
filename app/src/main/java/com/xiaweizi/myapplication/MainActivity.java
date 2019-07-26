1f75960c88d42e8a0d6dbba95403fbeb2faee0d9
xiaweizi <xiaweizi@xiaomi.com>
2019-07-26 09:51:51 +0800

    [dev] 添加客户端服务端入口

--- app/src/main/java/com/xiaweizi/myapplication/MainActivity.java
+++ app/src/main/java/com/xiaweizi/myapplication/MainActivity.java
@@ -1,129 +1,28 @@
 package com.xiaweizi.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.lang.ref.WeakReference;
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final int MSG = 1;
-    private MyHandler mHandler;
-    private int mCurrentProgress = 0;
-    private TransferProgressView mProgressView;
-    private SearchRipplesView mSearchView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mProgressView = findViewById(R.id.progress_view);
-        mSearchView = findViewById(R.id.search_view);
-        mHandler = new MyHandler(this);
-        mProgressView.initMode(false);
-
-        findViewById(R.id.bt_start).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.bt_client).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!mHandler.hasMessages(MSG)) {
-                    mHandler.sendEmptyMessage(MSG);
-                }
-                mProgressView.start();
+                startActivity(new Intent(MainActivity.this, ClientActivity.class));
             }
         });
-        findViewById(R.id.bt_complete).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.bt_client).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mHandler.hasMessages(MSG)) {
-                    mHandler.removeMessages(MSG);
-                }
-                mProgressView.complete();
+                startActivity(new Intent(MainActivity.this, ServerActivity.class));
             }
         });
-
-        findViewById(R.id.bt_resume).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!mHandler.hasMessages(MSG)) {
-                    mHandler.sendEmptyMessage(MSG);
-                }
-                mProgressView.resume();
-            }
-        });
-        findViewById(R.id.bt_pause).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mHandler.hasMessages(MSG)) {
-                    mHandler.removeMessages(MSG);
-                }
-                mProgressView.getPaddingStart();
-            }
-        });
-
-        findViewById(R.id.bt_interrupt).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mHandler.hasMessages(MSG)) {
-                    mHandler.removeMessages(MSG);
-                }
-                mProgressView.interrupt();
-            }
-        });
-
-        findViewById(R.id.bt_recover).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!mHandler.hasMessages(MSG)) {
-                    mHandler.sendEmptyMessage(MSG);
-                }
-                mProgressView.recover();
-            }
-        });
-
-        findViewById(R.id.bt_start_ripples).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSearchView.start();
-            }
-        });
-
-        findViewById(R.id.bt_stop_ripples).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSearchView.stop();
-            }
-        });
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    static class MyHandler extends Handler {
-        WeakReference<MainActivity> mActivity;
-
-        MyHandler(MainActivity activity) {
-            mActivity = new WeakReference<>(activity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            MainActivity theActivity = mActivity.get();
-            if (theActivity == null || theActivity.isFinishing()) {
-                return;
-            }
-            theActivity.mCurrentProgress ++;
-            if (theActivity.mCurrentProgress >= 100) {
-                theActivity.mCurrentProgress = 0;
-            }
-            theActivity.mProgressView.setProgress(theActivity.mCurrentProgress);
-            sendEmptyMessageDelayed(MSG, 80);
-        }
     }
 }
